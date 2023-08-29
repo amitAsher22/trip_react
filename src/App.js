b9acdc5e538c834f719bc38102b2c45d7076b9e5
amita <amitasher4@gmail.com>
2023-08-29 16:01:56 +0300

    deploy

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
-import Navbar from "./components/Navbar";
+
 import Home from "./routes/Home";
 import Service from "./routes/Service";
 import Contact from "./routes/Contact";
